bbde147ab74556f5cb2513684491180c081775a7
InSupportMH <michael.hallstrom@insupport.se>
2026-01-14 14:35:36 +0100

    First commit
    Medicine form, docker compose with postgres

---
+++ MedicineLog/Entities.cs
@@ -0,0 +1,26 @@
+﻿namespace MedicineLog
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class MedicationRegistration
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string FirstName { get; set; } = "";
+
+        [Required, StringLength(100)]
+        public string LastName { get; set; } = "";
+
+        [Required, StringLength(200)]
+        public string MedicationName { get; set; } = "";
+
+        [Range(1, 100000)]
+        public int Quantity { get; set; }
+
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+    }
+
+}
---
+++ MedicineLog/Models/MedicineRegViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace MedicineLog.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class MedicineRegViewModel
+    {
+        [Display(Name = "Förnamn")]
+        [Required(ErrorMessage = "Förnamn är obligatoriskt.")]
+        [StringLength(100)]
+        public string FirstName { get; set; } = "";
+
+        [Display(Name = "Efternamn")]
+        [Required(ErrorMessage = "Efternamn är obligatoriskt.")]
+        [StringLength(100)]
+        public string LastName { get; set; } = "";
+
+        [Display(Name = "Läkemedel")]
+        [Required(ErrorMessage = "Läkemedlets namn är obligatoriskt.")]
+        [StringLength(200)]
+        public string MedicationName { get; set; } = "";
+
+        [Display(Name = "Antal")]
+        [Required(ErrorMessage = "Antal är obligatoriskt.")]
+        [Range(1, 100000, ErrorMessage = "Antal måste vara minst 1.")]
+        public int? Quantity { get; set; }
+    }
+
+}
