c02e3eb46bac1e2ff97e15982a01c35386621d3f
InSupportMH <michael.hallstrom@insupport.se>
2026-01-28 09:55:31 +0100

    Minor changes

--- MedicineLog/Data/Entities/Terminal.cs
+++ MedicineLog/Data/Entities/Terminal.cs
@@ -20,8 +20,6 @@ namespace MedicineLog.Data.Entities
         public DateTimeOffset? LastSeenAt { get; set; }
 
         public ICollection<MedicineLogEntry> LogEntries { get; set; } = new List<MedicineLogEntry>();
-
-        // Device auth (recommended: store sessions/tokens separately, not a shared password)
         public ICollection<TerminalSession> Sessions { get; set; } = new List<TerminalSession>();
     }
 
